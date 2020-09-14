ad14205d22542f14bad2caf7adba4c0ddaec6e6e
david1999erazo <david.erazo2@correo.icesi.edu.co>
2020-09-14 17:41:23 -0500

    Delete systemVariables

--- simulationFrameWorkGUI/src/components/VariablesConfig/variables.vue
+++ simulationFrameWorkGUI/src/components/VariablesConfig/variables.vue
@@ -8,48 +8,28 @@
             <v-spacer></v-spacer>
           </v-toolbar>
           <v-card-text class="mx-auto" width="400" tilde>
-            <v-radio-group>
-              <div v-for="item in items" :key="item">
-                <input
-                  type="checkbox"
-                  :id="item.text"
-                  :name="item.text"
-                  v-model="itemSelected"
-                  :value="item.text"
-                />
-                <label>{{item.text}}</label>
-              </div>
-            </v-radio-group>
+            <h5> Por favor seleccione las variables que desea ver en la simulación</h5>
+            <v-row align="center" justify="center">
+
+                <v-radio-group >
+                  <div v-for="item in items" :key="item">
+                    <input
+                      type="checkbox"
+                      :id="item.text"
+                      :name="item.text"
+                      v-model="itemSelected"
+                      :value="item.text"
+                    />
+                    <label>{{item.text}}</label>
+                  </div>
+                </v-radio-group>
+
+            </v-row>
 
             <v-row align="center" justify="center">
               <v-col align="center" cols="12" md="6" sm="6">
                 <div class="my-3">
-                  <v-dialog v-model="dialog" persistent max-width="600px">
-                    <template v-slot:activator="{ on, attrs }">
-                      <v-btn color="primary" dark v-bind="attrs" v-on="on">Edit variables</v-btn>
-                    </template>
-                    <v-card>
-                      <v-card-title>
-                        <span class="headline">Edit Variables</span>
-                      </v-card-title>
-                      <v-card-text>
-                        <div v-for="index in itemSelected.length" :key="index">
-                          <v-text-field
-                            :v-model="itemSelected[index-1]"
-                            :value="itemSelected[index-1]"
-                          ></v-text-field>
-                        </div>
-                      </v-card-text>
-                      <v-card-actions>
-                        <v-spacer></v-spacer>
-                        <v-btn color="blue darken-1" text @click="dialog = false">Close</v-btn>
-                        <v-btn color="blue darken-1" text @click="dialog = false">Save</v-btn>
-                      </v-card-actions>
-                    </v-card>
-                  </v-dialog>
-                </div>
-                <div class="my-3">
-                  <v-btn color="primary" router :to="{path:'/systemvariables'}">Assign variables</v-btn>
+                  <v-btn color="primary" router :to="{path:'/canvas'}">Next</v-btn>
                 </div>
                 <div class="my-3">
                   <v-btn color="primary" router :to="{path:'/newproject'}">Go Back</v-btn>
@@ -62,6 +42,11 @@
     </v-row>
   </v-container>
 </template>
+
+
+
+
+
 
 <script>
 export default {
--- simulationFrameWorkGUI/src/router/index.js
+++ simulationFrameWorkGUI/src/router/index.js
@@ -4,7 +4,6 @@ import canvas from '../components/Canvas/Canvas';
 import Home from '../components/Home.vue';
 import newProject from '../components/CreateProject/createProject';
 import variables from '../components/VariablesConfig/variables';
-import systemVariables from '../components/VariablesConfig/systemVariables';
 
 Vue.use(VueRouter)
 
@@ -16,7 +15,6 @@ const routes = [
     {path: '/newproject', component: newProject},
     {path: '/canvas', component: canvas},
     {path: '/variables', component: variables},
-    {path: '/systemvariables', component: systemVariables}
 ];
 
 export default new VueRouter({
