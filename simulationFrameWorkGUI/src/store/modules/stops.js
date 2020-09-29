d153fce605c8124c448a498db5e0e842a8af4708
david1999erazo <david.erazo2@correo.icesi.edu.co>
2020-09-28 22:44:38 -0500

    Added Layers with Stops

--- simulationFrameWorkGUI/src/store/modules/stops.js
+++ simulationFrameWorkGUI/src/store/modules/stops.js
@@ -3,26 +3,25 @@ import axios from '../../helpers/api'
 // initial state
 const state ={
     stops:[],
-    flagGetStops:{}
 }
 
 // Getters
 const getters={
     getAllStops: state => state.stops,
-    getFlagGetStops: state=> state.getFlagGetStops
 }
 
 // Actions
 const actions = {
     loadStops: function({commit},payload){
-        axios.get('stops/',payload)
+        axios.request({url: '/datasource/stops',data:null, params: payload})
         .then(res => {
             commit('setStopsToState',res.data)
+            console.log(res.data)
         })
     }
 }
 
-//mutations 
+//mutations
 const mutations={
     setStopsToState(state,stops){
         state.stops= stops
