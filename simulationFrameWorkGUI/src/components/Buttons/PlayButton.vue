d9d33718109afb13493c743b0ffc91e50b172046
juangallooo <juanesteban3600@gmail.com>
2020-10-07 14:18:41 -0500

    Canvas update

--- simulationFrameWorkGUI/src/components/Buttons/PlayButton.vue
+++ simulationFrameWorkGUI/src/components/Buttons/PlayButton.vue
@@ -1,64 +1,170 @@
 <template>
-    <div class="play-pause-skip-controls">
-    <div @click="previous">
-      <font-awesome-icon class="play-pause-skip-controls__icons" size="2x" icon="arrow-left" />
-      
-    </div>
-    <div v-if="getNowPlayingStatus.isLoading || fakeLoading" class="spinner">
-    </div>
-    <div v-else-if="!isPlaying" @click="togglePlayPause">
-      <font-awesome-icon :icon="['fas', 'play']" class="play-pause-skip-controls__icons" size="2x" /> 
-    </div>
-    <div v-else @click="togglePlayPause">
-      <font-awesome-icon class="play-pause-skip-controls__icons" size="2x" icon="pause" />
-    </div>
-    <div @click="next">
-      <font-awesome-icon class="play-pause-skip-controls__icons" size="2x" icon="arrow-right" />
-    </div>
+  <div>
+    <v-row justify="space-around" align="center">
+      <div class="play-pause-skip-controls">
+        <div
+          v-if="getNowPlayingStatus.isLoading || fakeLoading"
+          class="spinner"
+        ></div>
+        <div v-else-if="!isPlaying" @click="togglePlayPause">
+          <font-awesome-icon
+            :icon="['fas', 'play']"
+            class="play-pause-skip-controls__icons"
+            size="2x"
+          />
+        </div>
+        <div v-else @click="togglePlayPause">
+          <font-awesome-icon
+            :icon="['fas', 'pause']"
+            class="play-pause-skip-controls__icons"
+            size="2x"
+          />
+        </div>
+      </div>
+    </v-row>
+    <v-row justify="space-around" align="left">
+      <v-col cols="6" align="left" justify="left"> Read speed </v-col>
+      <v-col cols="6" align="left" justify="left">
+        <v-slider v-model="slider" thumb-label="always"></v-slider>
+      </v-col>
+    </v-row>
+    <v-row justify="space-around" align="left">
+      <v-col cols="6" align="left" justify="left"> Simulation speed </v-col>
+      <v-col cols="6" align="left" justify="left">
+        <v-slider v-model="sliderTwo" thumb-label="always"></v-slider>
+      </v-col>
+      </v-row>
+    <v-data-table
+      v-model="selected"
+      :headers="headersPlanVersion"
+      :items="planversions"
+      :items-per-page="5"
+      :single-select="true"
+      item-key="name"
+      show-select
+      class="elevation-1"
+    >
+    </v-data-table>
+
+    <v-data-table
+      :headers="headers"
+      :items="desserts"
+      :items-per-page="5"
+      item-key="name"
+      class="elevation-1"
+      :footer-props="{
+        showFirstLastPage: true,
+        firstIcon: 'mdi-arrow-collapse-left',
+        lastIcon: 'mdi-arrow-collapse-right',
+        prevIcon: 'mdi-minus',
+        nextIcon: 'mdi-plus',
+      }"
+    ></v-data-table>
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'PlayButton',
+  name: "PlayButton",
   data() {
     return {
-      getNowPlayingStatus:{
-        isLoading:false,
+      slider: 45,
+      sliderTwo: 45,
+      getNowPlayingStatus: {
+        isLoading: false,
       },
-      fakeLoading:false,
-      isPlaying:false,
+      fakeLoading: false,
+      isPlaying: false,
+      headersPlanVersion:[
+        {
+          text: "Ids",
+          align: "start",
+          value: "planVersionId",
+        },
+      ],
+      headers: [
+        {
+          text: "Variables",
+          align: "start",
+          value: "name",
+        },
+        { text: "Values", value: "value" },
+      ],
+      planversions:[
+        
+      ],
+      desserts: [
+        {
+          name: "Frozen Yogurt",
+          value: "Ice cream",
+        },
+        {
+          name: "Ice cream sandwich",
+          value: "Ice cream",
+        },
+        {
+          name: "Eclair",
+          value: "Cookie",
+        },
+        {
+          name: "Cupcake",
+          value: "Pastry",
+        },
+        {
+          name: "Gingerbread",
+          value: "Cookie",
+        },
+        {
+          name: "Jelly bean",
+          value: "Candy",
+        },
+        {
+          name: "Lollipop",
+          value: "Candy",
+        },
+        {
+          name: "Honeycomb",
+          value: "Toffee",
+        },
+        {
+          name: "Donut",
+          value: "Pastry",
+        },
+        {
+          name: "KitKat",
+          value: "Candy",
+        },
+      ],
     };
   },
   methods: {
-    play: function(){
-
-
-    },
-    pause: function(){
-
-
-    },
-    next: function(){
-
-
-    },
-    previous: function(){
-
-
-    },
-    togglePlayPause: function(){
-
-
+    play: function () {},
+    pause: function () {},
+    next: function () {},
+    previous: function () {},
+    togglePlayPause: function () {
+      this.isPlaying = !this.isPlaying;
     },
   },
-  computed: {
+   mounted: function () {
+    if(this.$store.getters['projects/getAllPlanVersions'].length==0){
+      let payload= {type:"FileCSV"}
+      this.$store.dispatch('projects/loadPlanVersions',payload)
+    }else{
+      this.planversions=this.$store.getters['projects/getAllPlanVersions']
+        console.log(this.planversionID)
+    }
+    this.$store.subscribe( (mutation, state) => {
+      if (mutation.type === 'projects/setPlanVersions') {
+        this.planversions=this.$store.getters['projects/getAllPlanVersions']
+        console.log(this.planversions)
+      }
 
+    })
   },
-}
+  computed: {},
+};
 </script>
 
 <style>
-
 </style>
--- simulationFrameWorkGUI/src/components/Canvas/Canvas.vue
+++ simulationFrameWorkGUI/src/components/Canvas/Canvas.vue
@@ -1,22 +1,22 @@
 <template>
   <v-app id="canvas">
     <v-row align="center" justify="center">
-          <v-col cols="12" sm="8" md="4">
-            <sideBar></sideBar>
-          </v-col>
-          <v-col cols="12" sm="8" md="4">
-            <mapC></mapC>
-          </v-col>
-          <v-col cols="12" sm="8" md="4">
-            <play-button></play-button>
-          </v-col>
-        </v-row>
+      <v-col cols="12" sm="8" md="4">
+        <sideBar></sideBar>
+      </v-col>
+      <v-col cols="12" sm="8" md="4">
+        <mapC></mapC>
+      </v-col>
+      <v-col cols="12" sm="8" md="4">
+        <play-button></play-button>
+      </v-col>
+    </v-row>
   </v-app>
 </template>
 <script>
-import sideBar from '../SideMenu/SideMenu'
-import mapC from "../Map/mapCanvas"
-import playButton from "../Buttons/PlayButton"
+import sideBar from "../SideMenu/SideMenu";
+import mapC from "../Map/mapCanvas";
+import playButton from "../Buttons/PlayButton";
 export default {
   name: "Canvas",
   data() {
@@ -28,16 +28,15 @@ export default {
     mapC,
   },
   created() {
-    window.bus.$on('menu/toggle', () => {
+    window.bus.$on("menu/toggle", () => {
       window.setTimeout(() => {
         this.isOpenMobileMenu = !this.isOpenMobileMenu;
       }, 200);
     });
 
-    window.bus.$on('menu/closeMobileMenu', () => {
+    window.bus.$on("menu/closeMobileMenu", () => {
       this.isOpenMobileMenu = false;
     });
-
   },
 };
 </script>
